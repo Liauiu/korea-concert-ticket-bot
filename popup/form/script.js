bed8d2f26de982a2be6ea3147d5837c0aedf02b4
Liauiu <liuwyyye@gmail.com>
2025-05-11 16:47:44 +1000

    Update script.js

--- popup/form/script.js
+++ popup/form/script.js
@@ -1,27 +1,26 @@
 import { get_stored_value, store_value } from "../module/storage.js";
 
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", () => {
   const form = document.querySelector("form");
 
-  form.addEventListener("submit", async function (event) {
-    event.preventDefault();
-    form.getElementsByTagName("button")[0].disabled = true;
+  form.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    form.querySelector("button").disabled = true;
 
-    let data = {};
     const formData = new FormData(form);
+    const data = {};
     for (const [key, value] of formData.entries()) {
       data[key] = value;
     }
-    data["section"] = data["section"].split(",");
-    data["first-section"] = data["first-section"].split(",");
-    data["second-section"] = data["second-section"].split(",");
-    data["platform"] = form.getElementsByTagName("button")[0].id;
 
-    let array = (await get_stored_value("autoBooking")) || [];
+    data["first-section"] = data["first-section"].split(",").map(s => s.trim());
+    data["second-section"] = data["second-section"]?.split(",").map(s => s.trim()) || [];
+    data["platform"] = form.querySelector("button").id;
+
+    const array = await get_stored_value("autoBooking") || [];
     store_value(data["concert-id"], data);
     array.push(data);
     store_value("autoBooking", array);
-
     window.history.back();
   });
 });
