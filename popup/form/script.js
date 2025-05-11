42d8c4d1810d364b942cef81db920baca28007a6
Liauiu <liuwyyye@gmail.com>
2025-05-11 16:33:46 +1000

    Update script.js

--- popup/form/script.js
+++ popup/form/script.js
@@ -1,36 +1,27 @@
 import { get_stored_value, store_value } from "../module/storage.js";
 
-window.onclick = function(event) {
-    const target = event.target;
-    if (target.classList.contains("close")) {
-        window.history.back();
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.querySelector("form");
+
+  form.addEventListener("submit", async function (event) {
+    event.preventDefault();
+    form.getElementsByTagName("button")[0].disabled = true;
+
+    let data = {};
+    const formData = new FormData(form);
+    for (const [key, value] of formData.entries()) {
+      data[key] = value;
     }
-};
+    data["section"] = data["section"].split(",");
+    data["first-section"] = data["first-section"].split(",");
+    data["second-section"] = data["second-section"].split(",");
+    data["platform"] = form.getElementsByTagName("button")[0].id;
 
-document.addEventListener('DOMContentLoaded', function () {
-    const form = document.querySelector('form');
+    let array = (await get_stored_value("autoBooking")) || [];
+    store_value(data["concert-id"], data);
+    array.push(data);
+    store_value("autoBooking", array);
 
-    form.addEventListener('submit', async function (event) {
-        event.preventDefault();
-        form.getElementsByTagName("button")[0].disabled = true;
-
-        let data = {};
-        const formData = new FormData(form);
-        for (const [key, value] of formData.entries()) {
-            data[key] = value;
-        }
-
-        data["section"] = (data["section"] || "").split(",").map(s => s.trim());
-        data["first-section"] = (data["first-section"] || "").split(",").map(s => s.trim());
-        data["second-section"] = (data["second-section"] || "").split(",").map(s => s.trim());
-
-        data["platform"] = form.getElementsByTagName("button")[0].id;
-
-        let array = await get_stored_value("autoBooking") || [];
-        store_value(data["concert-id"], data);
-        array.push(data);
-        store_value("autoBooking", array);
-
-        window.history.back();
-    });
+    window.history.back();
+  });
 });
